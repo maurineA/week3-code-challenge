b4efece619d5dfa194a0e9faadcc5cceac1e312d
Mwambui123 <maurinawambuia@gmail.com>
2023-10-24 21:39:14 +0300

    Created licence and README.md files

---
+++ README.md
@@ -0,0 +1,65 @@
+# WEEK3-CODE-CHALLENGE
+
+This web application is used for buying movie tickets, OCTOBER 24/2023
+
+By Maurine Wambui
+
+# Description
+
+This web application displays movies available for purchase, details for every movie including the title, showtime, available tickets, capacity of the place the movie is taking place and a short description. 
+
+# How to Use
+
+<ul>
+<li>View the application on the browswer, you can go live just below the terminal and open in browser</li>
+<li>http://localhost:3000/films this is the current localhost server used in this project</li>
+<li>Start the server through json-server --watch db.json</li>
+<li>The movies will display in the browser</li>
+<li>The user can see the displayed movies on the browser</li>
+
+</ul>
+
+# Requirements
+
+<ul>
+<li>A computer with a bash terminal and node installed.</li>
+<li>Access to the internet.</li>
+</ul>
+
+# Installation Process
+
+<ul>
+<li>Clone this repository using
+
+  git clone https://github.com/maurineA/week3-code-challenge
+or by downloading a ZIP file of the code.</li>
+<li>The repository, if downloaded as a .zip file will need to be extracted to your preferred location.</li>
+<li>Navigate to the project folder on your bash terminal.</li>
+<li>Open the live server</li>
+<li>Start the server through json-server --watch db.json</li>
+</ul>
+
+  # Technologies Used
+<ul>
+<li>HTML</li>
+<li>CSS</li>
+<li>JS</li>
+<li>Node Runtime Environment</li>
+</ul>
+
+
+# Support and Contact Details
+
+Incase of any query, need for collaboration or issues with this code, feel free to reach me at maurine.wambui@student.moringaschool.com
+
+# License
+
+MIT License
+
+Copyright © 2023 Maurine Wambui 
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
--- js/script.js
+++ js/script.js
@@ -1,45 +1,75 @@
 let films = [];
+
 document.addEventListener("DOMContentLoaded", function(){
-    getFilms()
-})
+    getFilms();
+    
+});
 
 function getFilms(){
-    fetch('http://localhost:3002/films')
+    fetch('http://localhost:3000/films')
     .then(response => response.json())
     .then(data => {
-        films = [...data]
-        displayItems(data)
-    })
+        films = [...data];
+        displayItems(data);
+    });
 }
 
-
 function displayItems(films){
+
     const itemContainer = document.querySelector("#films");
     for(film of films){
-        itemContainer.innerHTML += `
+        itemContainer.innerHTML +=`
         <div class="p-2 m-1 col-3">
             <div class="card" >
                 <img src="${film.poster}" height="250px" class="card-img-top image" alt="...">
                 <div class="card-body">
-                <h5 class="card-title">${film.title}</h5>
-                <span class="card-text">
-                <ul>
-                 <li>Runtime: ${film.runtime}</li>
-                 <li>Capacity: ${film.capacity}</li>
-                 <li>Showtime: ${film.showtime}</li>
-                 <li>Tickets_sold: ${film.tickets_sold}</li>
-                 <li>Description: ${film.description}</li>
-                 <li>Completed: ${film.completed}</li>
-                </ul>
-                </span>
-                <form>
-                    <label>Buy</label>
-                    <input type="number" min="1" value="1"/>
-                    <button class="btn btn-primary btn-block m-2 btn-lg">Buy Ticket</button>
-                </form>
+                    <h5 class="card-title">${film.title}</h5>
+                    <span class="card-text">
+                        <ul>
+                            <li>Runtime: ${film.runtime}</li>
+                            <li>Capacity: ${film.capacity}</li>
+                            <li>Showtime: ${film.showtime}</li>
+                            <li>Tickets Sold: <span id="tickets-sold-${film.id}">${film.tickets_sold}</span></li>
+                            <li>Description: ${film.description}</li>
+                            <li>Completed: ${film.completed}</li>
+                        </ul>
+                    </span>
+                
+                        <button id="btn"class="btn btn-primary btn-block m-2 btn-lg">Buy Ticket</button>
+                        
+                    
                 </div>
-            </div>6
+            </div>
         </div>
-        `
+        `;
+
+        const btn = itemContainer.querySelector("#btn")
+        //console.log(btn)
+        btn.addEventListener('click', () =>{
+
+            console.log("i'm clicked")
+
+            fetch(`http://localhost:3000/films/${film.id}`,{
+                method: "PATCH",
+                headers: {
+                    "content-Type": "application/json",
+                    "Accept": "application/json"
+
+                },
+                body:JSON.stringify({
+
+                    tickets_sold:film.tickets_sold +1
+
+                })
+
+            })
+           
+        })
+
+        
+
+       
+
     }
 }
+    
